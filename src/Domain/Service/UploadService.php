7107098b67154b7d68577b023915b087fb25bd38
Vladimir Volkov <volkov_vd@drom.ru>
2021-10-13 12:04:54 +1000

    Изменить алгоритм хэширования

--- src/Domain/Service/UploadService.php
+++ src/Domain/Service/UploadService.php
@@ -26,11 +26,11 @@ class UploadService
      * @throws ORMException
      */
     public function upload(UploadedFile $file): ?AudioFile {
-        $hash = md5($file->getContent());
+        $hash = sha1($file->getContent());
         if ($this->audioFileRepository->findOneBy(['hash' => $hash])) {
             return null;
         }
-        $name = md5($file->getClientOriginalName() . 'Md5F47wn53eoZ3d');
+        $name = sha1($file->getClientOriginalName() . 'Md5F47wn53eoZ3d');
         $audioFile = new AudioFile($this->uploadDir . $name . ".wav", $hash);
         $file->move($this->uploadDir, "$name.wav");
         $this->audioFileRepository->save($audioFile);
